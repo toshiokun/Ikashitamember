dac6036b43024ac3db093288685cf97a462488e6
hataiga <mbpr.tiger@gmail.com>
2015-09-11 18:02:39 +0900

    refs #38502 Offerモデルを作成いたしました。

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909112155) do
+ActiveRecord::Schema.define(version: 20150911085128) do
+
+  create_table "offers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "recruit_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"

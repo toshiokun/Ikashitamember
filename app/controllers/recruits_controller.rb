fe6c6b3deb3ac7167f812b9cfe862bc14536df1c
hataiga <mbpr.tiger@gmail.com>
2015-09-12 16:05:33 +0900

    Recruitのscaffoldを作成しました

---
+++ app/controllers/recruits_controller.rb
@@ -0,0 +1,74 @@
+class RecruitsController < ApplicationController
+  before_action :set_recruit, only: [:show, :edit, :update, :destroy]
+
+  # GET /recruits
+  # GET /recruits.json
+  def index
+    @recruits = Recruit.all
+  end
+
+  # GET /recruits/1
+  # GET /recruits/1.json
+  def show
+  end
+
+  # GET /recruits/new
+  def new
+    @recruit = Recruit.new
+  end
+
+  # GET /recruits/1/edit
+  def edit
+  end
+
+  # POST /recruits
+  # POST /recruits.json
+  def create
+    @recruit = Recruit.new(recruit_params)
+
+    respond_to do |format|
+      if @recruit.save
+        format.html { redirect_to @recruit, notice: 'Recruit was successfully created.' }
+        format.json { render :show, status: :created, location: @recruit }
+      else
+        format.html { render :new }
+        format.json { render json: @recruit.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /recruits/1
+  # PATCH/PUT /recruits/1.json
+  def update
+    respond_to do |format|
+      if @recruit.update(recruit_params)
+        format.html { redirect_to @recruit, notice: 'Recruit was successfully updated.' }
+        format.json { render :show, status: :ok, location: @recruit }
+      else
+        format.html { render :edit }
+        format.json { render json: @recruit.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /recruits/1
+  # DELETE /recruits/1.json
+  def destroy
+    @recruit.destroy
+    respond_to do |format|
+      format.html { redirect_to recruits_url, notice: 'Recruit was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_recruit
+      @recruit = Recruit.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def recruit_params
+      params.require(:recruit).permit(:name, :member, :place, :activity, :music_instrument)
+    end
+end
--- app/models/recruit.rb
+++ app/models/recruit.rb
@@ -1,9 +1,2 @@
 class Recruit < ActiveRecord::Base
-<<<<<<< HEAD
-  belongs_to :user
-=======
-
-  belongs_to :user_id
-
->>>>>>> 5555554c050455ad834b7c508c69a2392c036733
 end

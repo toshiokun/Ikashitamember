48d39f736243375fad0e22ccfd994b0492d24638
Ryo Kageyama <ryo.kageyama@gmail.com>
2015-09-15 07:13:23 +0000

    signinできるようにする

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912070010) do
+ActiveRecord::Schema.define(version: 20150915070419) do
 
   create_table "applies", force: :cascade do |t|
     t.integer  "user_id"
@@ -50,6 +50,10 @@ ActiveRecord::Schema.define(version: 20150912070010) do
     t.string   "music_instrument"
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
+    t.string   "remember_token"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end

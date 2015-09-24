8a436bb5eac1aae753f7b1f9b8c123128eb3297a
Toshihiro Taniguchi <10401040.taniyama@gmail.com>
2015-09-24 20:12:45 +0900

    refs #40025 signinしたらリクルート一覧に飛ぶように変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user=User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_to recruits_path
     else
       flash.new[:error] = 'Invalid email/password combination'
       render 'new'
